0ff16dab948a586f11e35ce2debf2c77e7007296
Evan Moon <bboydart91@gmail.com>
2017-08-13 18:59:58 +0900

    Content component has updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-    {
-        path: '',
-        children: []
-    }
-];
+import { ContentsComponent } from './contents/contents.component';
+
+const routes:Routes = [{
+    path: '',
+    children: []
+},{
+    path: 'contents',
+    component: ContentsComponent
+}];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
