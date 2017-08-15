a2be14e0c99f6c8bf504bdea8efcfc66f6e66a8a
Evan Moon <bboydart91@gmail.com>
2017-08-15 16:32:23 +0900

    webpack config has updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContentsComponent } from './contents/contents.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        ContentsComponent
+        AppComponent
     ],
     imports: [
         BrowserModule.withServerTransition({appId: 'pixelstairs'}),
