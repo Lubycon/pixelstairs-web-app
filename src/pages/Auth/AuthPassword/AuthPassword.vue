cc0bf8f10d3e61f0a5035cf93a1cdb0d1ea59002
Evan Moon <bboydart91@gmail.com>
2017-09-23 13:30:15 +0900

    password setting flow style has been updated

--- src/pages/Auth/AuthPassword/AuthPassword.vue
+++ src/pages/Auth/AuthPassword/AuthPassword.vue
@@ -1,18 +1,20 @@
 <template>
-<div class="container">
-    <h1>Password check page</h1>
-    <b-form @submit.prevent="submit">
-        <b-form-group label="Password">
-            <b-form-input
-                type="password"
-                placeholder="password"
-                v-model="password"
-            />
-        </b-form-group>
-        <b-button type="submit" variant="primary">
-            Submit
-        </b-button>
-    </b-form>
+<div class="container row">
+    <div class="col-12 col-md-6 col-lg-4 card card--center-form">
+        <h1>Please enter your password</h1>
+        <b-form @submit.prevent="submit">
+            <b-form-group>
+                <b-form-input
+                    type="password"
+                    placeholder="Password"
+                    v-model="password"
+                />
+            </b-form-group>
+            <b-button type="submit" variant="primary">
+                Next
+            </b-button>
+        </b-form>
+    </div>
 </div>
 </template>
 
--- src/pages/User/Setting/UserPassword/UserPassword.vue
+++ src/pages/User/Setting/UserPassword/UserPassword.vue
@@ -1,18 +1,23 @@
 <template>
-<div class="container">
-    <h1>User Password Change</h1>
-    <b-form @submit.prevent="submit">
-        <b-form-group label="New Password">
-            <b-form-input
-                type="password"
-                placeholder="New Password"
-                v-model="password"
-            />
-        </b-form-group>
-        <b-button type="submit" variant="primary">
-            Submit
-        </b-button>
-    </b-form>
+<div class="container row">
+    <div class="col-12 col-md-6 col-lg-4 card card--center-form">
+        <h1>We're ready for getting your new password!</h1>
+        <b-form @submit.prevent="submit">
+            <b-form-group>
+                <b-form-input
+                    type="password"
+                    placeholder="New Password"
+                    v-model="password"
+                />
+            </b-form-group>
+            <b-button type="submit" variant="primary">
+                Change
+            </b-button>
+            <b-button type="button" class="btn-border" :to="{ name: 'home' }">
+                Cancel
+            </b-button>
+        </b-form>
+    </div>
 </div>
 </template>
 
